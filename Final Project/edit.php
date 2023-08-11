4d19d66d55cab05b4712ed1190f9e77d41839459
Stephen Hill <stephenhill30@gmail.com>
2023-08-11 07:24:22 -0400

    Update edit.php

--- Final Project/edit.php
+++ Final Project/edit.php
@@ -1,3 +1,18 @@
 <?php
-    if(isset($_POST['row_id']))
+require './inc/header.php';
+require './inc/database.php';
+//  Save the current row that redirected us here
+$current_row = $_POST['row_id'];
+if(isset($_POST['row_id']))  {
+//    SQL to display the current row we are editing
+    $sql = 'SELECT FROM userList WHERE ID = ' . $current_row;
+    $result = $conn->query($sql);
+
+    foreach($result as $row) {
+
+    }
+
+}
+require './inc/footer.php';
 ?>
+
